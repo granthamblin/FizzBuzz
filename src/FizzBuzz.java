32a5dce9bf764281d77351ff76bc4825d48f65e6
Grant <143639424+granthamblin@users.noreply.github.com>
2024-05-16 18:38:16 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,8 +3,7 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
-
+    static void FizzBuzzForLoop() {
         for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
@@ -13,7 +12,6 @@ class FizzBuzz {
 
             // Print our appropriate result.
             if (divisibleBy3 && divisibleBy5) {
-
                 System.out.println("Fizz Buzz");
 
             } else if (divisibleBy3) {
@@ -29,6 +27,57 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+        }
+    }
+
+    static void FizzBuzzWhileLoop(){
+        int i = 1;
+
+        while (i < 100) {
+
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 && divisibleBy5) {
+                System.out.println("Fizz Buzz");
+            } else if (divisibleBy3) {
+                System.out.println("Fizz");
+            } else if (divisibleBy5) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            doFizzBuzz(i);
+        }
+    }
+
+    private static void doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
         }
     }
 }
